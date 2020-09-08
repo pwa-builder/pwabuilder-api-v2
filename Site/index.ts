7b78cf7ae3b3dce41dd7586c9ea7067ca5587b52
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-08 15:05:53 -0700

    format, start to integrate the pwabuiler-lib tools

--- Site/index.ts
+++ Site/index.ts
@@ -1,29 +1,52 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import * as puppeteer from "puppeteer";
-import { ifFile } from './helpers';
+import { ifFile } from "./helpers";
+import * as site from "./site";
+import { detect } from "pwabuilder-lib/lib/manifestTools";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const site = req.query.site;
-    const isFile = req.method === "POST" && ifFile(req)
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  let browser: puppeteer.Browser;
+  const isFile = req.method === "POST" && ifFile(req);
 
-    if (isFile) {
-        // const file = req.body;
-        context.res = {
-            status: 400,
-            body: {
-                message: "not supported yet"
-            }
-        }
-    }
-
+  // Handle File
+  if (isFile) {
+    // const file = req.body;
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            a: true
-        }
+      status: 400,
+      body: {
+        message: "not supported yet",
+      },
     };
+  }
 
+  // Handle Site
+  try {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    const siteUrl = req.query.site;
+    const manifest = await site.getManifest(browser, siteUrl);
+  } catch (e) {
+    // if (e.message === site.Errors.)
+
+    console.log(e);
+  } finally {
+    if (browser) {
+      browser.close();
+    }
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: {
+      a: true,
+    },
+  };
 };
 
 export default httpTrigger;
