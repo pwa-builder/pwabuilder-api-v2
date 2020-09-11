7c997b9cac0427021f6f989c519da0601ecf38c4
Justin Willis <jgw9617@gmail.com>
2020-09-11 11:04:15 -0700

    refactor(): use manifest utils function to get manifest to test

--- WebManifest/index.ts
+++ WebManifest/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import fetch from 'node-fetch';
 import * as puppeteer from 'puppeteer';
 import { checkBackgroundColor, checkCategories, checkDesc, checkDisplay, checkIcons, checkMaskableIcon, checkName, checkOrientation, checkRating, checkRelatedApps, checkRelatedPref, checkScreenshots, checkShortName, checkStartUrl, checkThemeColor } from './mani-tests';
+import getManifest from "../utils/getManifest";
 
 
 
@@ -18,38 +18,29 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
       }
     );
-    const page1 = await browser.newPage();
 
-    await page1.goto(site);
+    const maniData = await getManifest(browser, site);
 
-    const manifestHref = await page1.$eval('link[rel=manifest]', (el: HTMLAnchorElement) => el.href);
-
-    await page1.close();
-
-    if (manifestHref) {
-
-      const response = await fetch(manifestHref);
-      const maniData = await response.json();
-
+    if (maniData) {
       const results = {
         "required": {
-          "short_name": checkShortName(maniData),
-          "name": checkName(maniData),
-          "display": checkDisplay(maniData),
-          "start_url": checkStartUrl(maniData),
-          "icons": checkIcons(maniData)
+          "short_name": checkShortName(maniData.json),
+          "name": checkName(maniData.json),
+          "display": checkDisplay(maniData.json),
+          "start_url": checkStartUrl(maniData.json),
+          "icons": checkIcons(maniData.json)
         },
         "recommended": {
-          "screenshots": checkScreenshots(maniData),
-          "description": checkDesc(maniData),
-          "categories": checkCategories(maniData),
-          "maskable_icon": checkMaskableIcon(maniData),
-          "iarc_rating": checkRating(maniData),
-          "related_applications": checkRelatedApps(maniData),
-          "prefer_related_applications": checkRelatedPref(maniData),
-          "background_color": checkBackgroundColor(maniData),
-          "theme_color": checkThemeColor(maniData),
-          "orientation": checkOrientation(maniData)
+          "screenshots": checkScreenshots(maniData.json),
+          "description": checkDesc(maniData.json),
+          "categories": checkCategories(maniData.json),
+          "maskable_icon": checkMaskableIcon(maniData.json),
+          "iarc_rating": checkRating(maniData.json),
+          "related_applications": checkRelatedApps(maniData.json),
+          "prefer_related_applications": checkRelatedPref(maniData.json),
+          "background_color": checkBackgroundColor(maniData.json),
+          "theme_color": checkThemeColor(maniData.json),
+          "orientation": checkOrientation(maniData.json)
         },
         "optional": {
 
@@ -61,14 +52,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         body: {
           "data": results,
           "content": maniData
-        }
-      }
-    }
-    else {
-      context.res = {
-        status: 400,
-        body: {
-          "error": "Manifest could not be found"
         }
       }
     }
