45b41290e69a60d465c4591e608e94397ba4e31f
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-11 17:51:51 -0700

    new exception

--- utils/Exception.ts
+++ utils/Exception.ts
@@ -1,9 +1,11 @@
 export enum ExceptionType {
   MANIFEST_NOT_FOUND = "MANIFEST_NOT_FOUND",
+  MANIFEST_FILE_UNSUPPORTED = "MANIFEST_FILE_UNSUPPORTED",
 }
 
 export enum ExceptionMessage {
-  MANIFEST_NOT_FOUND = "failed to find the manifest"
+  MANIFEST_NOT_FOUND = "failed to find the manifest",
+  MANIFEST_FILE_UNSUPPORTED = "failed to read the json of the submitted manifest file",
 }
 
 /*
