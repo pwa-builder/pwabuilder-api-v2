fd1355ce225b5d9613a2d4462787ed6588c6f15c
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-15 18:23:40 -0700

    rename store to queue, due to it being assoc. closely and it is the blob interactions of the platform end point

--- PlatformQueue/index.ts
+++ PlatformQueue/index.ts
@@ -3,6 +3,11 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 const httpTrigger: AzureFunction = async function (
   context: Context
 ): Promise<void> {
+  context.log("Queue Item: " + context.bindings.queueItem);
+  context.log("Store Read: " + context.bindings.storeRead);
+  context.log("Store Write: " + context.bindings.storeWrite);
+  context.bindings.storeWrite = context.bindings.storeRead;
+  context.done();
   // context.bindings.queueItem;
   // context.bindings.storeRead;
   // context.bindings.storeWrite;
