6763aa0f038d85cc72658a4e93dc349e92e13611
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-17 10:09:53 -0700

    config shape

--- config/index.ts
+++ config/index.ts
@@ -1,1 +1,15 @@
-export default require(`./${process.env.NODE_ENV || "development"}`);
+type apiUrl = string;
+
+interface Config {
+  azure: {
+    account_name: string;
+    access_key: string;
+  };
+  services: {
+    generateImages: apiUrl;
+  };
+}
+
+export default <Config>(
+  require(`./${process.env.NODE_ENV || "development"}`).default
+);
