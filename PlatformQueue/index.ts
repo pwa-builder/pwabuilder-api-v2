3f00d0ccfbeecba320820d975fe470531279bc07
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-17 10:39:05 -0700

    do I need to commit for the publish?

--- PlatformQueue/index.ts
+++ PlatformQueue/index.ts
@@ -1,9 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const httpTrigger: AzureFunction = async function (
-  context: Context
+  context: Context,
+  manifest: Manifest.w3c
 ): Promise<void> {
   context.log("Queue Item: " + context.bindings.queueItem);
+  context.log("Manifest Additional Input: " + manifest);
+  context.log("Manifest Binding: " + context.bindings.manifest);
   context.log("Store Read: " + context.bindings.storeRead);
   context.log("Store Write: " + context.bindings.storeWrite);
   context.bindings.storeWrite = context.bindings.storeRead;
