22d86ed07e5d2e2785aac1f7dda4997ae34bfc2c
Justin Willis <jgw9617@gmail.com>
2020-09-18 10:40:22 -0700

    refactor(): manifest function can accept raw manifest object

--- WebManifest/index.ts
+++ WebManifest/index.ts
@@ -6,30 +6,32 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   context.log('Web Manifest function processed a request.');
 
   const site = req.query.site;
+  
+  const maniObject = req.body.manifest;
+  const maniUrl = req.body.maniurl;
+
   try {
 
-    const maniData = await getManifest(site);
-
-    if (maniData) {
+    if (maniObject) {
       const results = {
         "required": {
-          "short_name": checkShortName(maniData.json),
-          "name": checkName(maniData.json),
-          "display": checkDisplay(maniData.json),
-          "start_url": checkStartUrl(maniData.json),
-          "icons": checkIcons(maniData.json)
+          "short_name": checkShortName(maniObject),
+          "name": checkName(maniObject),
+          "display": checkDisplay(maniObject),
+          "start_url": checkStartUrl(maniObject),
+          "icons": checkIcons(maniObject)
         },
         "recommended": {
-          "screenshots": checkScreenshots(maniData.json),
-          "description": checkDesc(maniData.json),
-          "categories": checkCategories(maniData.json),
-          "maskable_icon": checkMaskableIcon(maniData.json),
-          "iarc_rating": checkRating(maniData.json),
-          "related_applications": checkRelatedApps(maniData.json),
-          "prefer_related_applications": checkRelatedPref(maniData.json),
-          "background_color": checkBackgroundColor(maniData.json),
-          "theme_color": checkThemeColor(maniData.json),
-          "orientation": checkOrientation(maniData.json)
+          "screenshots": checkScreenshots(maniObject),
+          "description": checkDesc(maniObject),
+          "categories": checkCategories(maniObject),
+          "maskable_icon": checkMaskableIcon(maniObject),
+          "iarc_rating": checkRating(maniObject),
+          "related_applications": checkRelatedApps(maniObject),
+          "prefer_related_applications": checkRelatedPref(maniObject),
+          "background_color": checkBackgroundColor(maniObject),
+          "theme_color": checkThemeColor(maniObject),
+          "orientation": checkOrientation(maniObject)
         },
         "optional": {
 
@@ -40,7 +42,48 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         status: 200,
         body: {
           "data": results,
-          "content": maniData
+          "content": {
+            json: maniObject,
+            url: maniUrl || site
+          }
+        }
+      }
+    }
+    else {
+      const maniData = await getManifest(site);
+
+      if (maniData) {
+        const results = {
+          "required": {
+            "short_name": checkShortName(maniData.json),
+            "name": checkName(maniData.json),
+            "display": checkDisplay(maniData.json),
+            "start_url": checkStartUrl(maniData.json),
+            "icons": checkIcons(maniData.json)
+          },
+          "recommended": {
+            "screenshots": checkScreenshots(maniData.json),
+            "description": checkDesc(maniData.json),
+            "categories": checkCategories(maniData.json),
+            "maskable_icon": checkMaskableIcon(maniData.json),
+            "iarc_rating": checkRating(maniData.json),
+            "related_applications": checkRelatedApps(maniData.json),
+            "prefer_related_applications": checkRelatedPref(maniData.json),
+            "background_color": checkBackgroundColor(maniData.json),
+            "theme_color": checkThemeColor(maniData.json),
+            "orientation": checkOrientation(maniData.json)
+          },
+          "optional": {
+
+          }
+        }
+
+        context.res = {
+          status: 200,
+          body: {
+            "data": results,
+            "content": maniData
+          }
         }
       }
     }
