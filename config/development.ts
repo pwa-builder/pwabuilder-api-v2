9e892e8d2cf79c04389278b6d6d36e6b04186541
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-22 19:26:02 -0700

    unused atm

--- config/development.ts
+++ config/development.ts
@@ -1,11 +1,8 @@
 export default {
   azure: {
-    // account_name: "manifolddev",
-    account_name: "vleonle-pwabuilder-api-v2",
-    // access_key:
-    //   "y4nxuSBfRtukWKATRZR7Ji3zx+6hEtAGUwKxUQmuUY7q94lp1NqO453nNbiX/tYg7xnPUSojXMY8lQ5xJqClmw==",
+    account_name: "manifolddev",
     access_key:
-      "Mr0Z1EBkalo9VVYrSMcpx4k2akgZt/MgqWFu19oTDhKVOQ1eLqwKurqhWd77Xn8BF/s1wok7Im/SJaRvXSmRoA==",
+      "y4nxuSBfRtukWKATRZR7Ji3zx+6hEtAGUwKxUQmuUY7q94lp1NqO453nNbiX/tYg7xnPUSojXMY8lQ5xJqClmw==",
   },
   services: {
     generateImages: process.env.IMG_GEN_SVC_URL || "http://localhost:49080/",
