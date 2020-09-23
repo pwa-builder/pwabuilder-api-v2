03a909dff672588b5b3c1a07b667a574fd3289d1
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-23 14:56:53 -0700

    start

---
+++ PlatformDownloadImage/index.ts
@@ -0,0 +1,28 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ *
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+import { AzureFunction, Context } from "@azure/functions";
+import { ExceptionWrap } from "../utils/Exception";
+import { getBlobServiceClient } from "../utils/storage";
+
+const activityFunction: AzureFunction = async function (
+  context: Context,
+  url: string
+): Promise<string> {
+  try {
+    const blobServiceClient = getBlobServiceClient();
+  } catch (exception) {
+    if (exception instanceof ExceptionWrap) {
+    }
+  }
+};
+
+export default activityFunction;
