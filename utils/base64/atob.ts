f595633c42bcaf5018960294cf495cf1e30e0e43
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-24 10:22:46 -0700

    handle node fetch response if from Buffer, better convenience

--- utils/base64/atob.ts
+++ utils/base64/atob.ts
@@ -1,5 +1,9 @@
 type Base64String = string;
 
-export default function atob(str: string): Base64String {
-  return Buffer.from(str, "base64").toString("base64");
+export default function atob(str: string | Buffer): Base64String {
+  if (str instanceof Buffer) {
+    return str.toString("base64");
+  }
+
+  return Buffer.from(str as string, "base64").toString("base64");
 }
