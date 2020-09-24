3da6144dfcead60733301178786a2e1e246f4588
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-24 12:07:07 -0700

    clean up cron

--- PlatformDeleteCron/index.ts
+++ PlatformDeleteCron/index.ts
@@ -7,29 +7,13 @@ const timerTrigger: AzureFunction = async function (
 ): Promise<void> {
   if (myTimer.isPastDue) {
     const blobServiceClient = getBlobServiceClient();
-    const promiseList = [];
     for await (const container of blobServiceClient.listContainers()) {
       if (
         new Date(container.properties.lastModified).getTime() <
           new Date(myTimer.Last).getTime() &&
         container.metadata["isSiteData"]
       ) {
-        promiseList.push(blobServiceClient.deleteContainer(container.name));
-      }
-    }
-
-    // LTS 12
-    if ("allSettled" in Promise) {
-      return (<any>Promise).allSettled(promiseList);
-    } else {
-      // LTS 10
-
-      for (const promise of promiseList) {
-        try {
-          await promise;
-        } catch (e) {
-          /* Ignored */
-        }
+        await blobServiceClient.deleteContainer(container.name);
       }
     }
   }
