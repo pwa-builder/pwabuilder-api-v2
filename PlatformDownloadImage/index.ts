bf0024affb125aa76e29a87bebc3005a5bc037e7
lee-leonardo <lee.leonardo.king@gmail.com>
2020-09-24 10:33:45 -0700

    download code

--- PlatformDownloadImage/index.ts
+++ PlatformDownloadImage/index.ts
@@ -10,19 +10,65 @@
  */
 
 import { AzureFunction, Context } from "@azure/functions";
-import { ExceptionWrap } from "../utils/Exception";
+import ExceptionOf, { ExceptionType, ExceptionWrap } from "../utils/Exception";
 import { getBlobServiceClient } from "../utils/storage";
+import atob from "../utils/base64/atob";
+import * as path from "path";
+import fetch from "node-fetch";
+import { BlockBlobUploadResponse } from "@azure/storage-blob";
+
+interface PlatformImageData {
+  containerId: string;
+  imageUrl: string;
+  tags: Array<string>;
+}
+
+interface PlatformImageTaskResponse {
+  blobRes?: BlockBlobUploadResponse;
+  success: boolean;
+  error?: ExceptionWrap;
+}
 
 const activityFunction: AzureFunction = async function (
   context: Context,
-  url: string
-): Promise<string> {
+  imageData: PlatformImageData
+): Promise<PlatformImageTaskResponse> {
+  let error;
   try {
     const blobServiceClient = getBlobServiceClient();
+    const containerClient = blobServiceClient.getContainerClient(
+      imageData.containerId
+    );
+    const [category, sizes, type, ...rest] = imageData.tags;
+    const fileName = path.parse(imageData.imageUrl).base;
+
+    const imageResponse = await fetch(imageData.imageUrl);
+    const imageBase64 = atob(imageResponse.body.read());
+
+    const uploadResponse = await containerClient.uploadBlockBlob(
+      fileName,
+      imageBase64,
+      imageBase64.length,
+      {
+        metadata: {
+          category,
+          sizes,
+          type,
+        },
+      }
+    );
+    return {
+      blobRes: uploadResponse.response,
+      success: true,
+    };
   } catch (exception) {
-    if (exception instanceof ExceptionWrap) {
-    }
+    error = ExceptionOf(ExceptionType.BLOB_STORAGE_FAILURE_IMAGE, exception);
   }
+
+  return {
+    success: false,
+    error: error,
+  };
 };
 
 export default activityFunction;
