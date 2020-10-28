714c73927aed3d243e4062b9a6cb75f8c0a0aa79
Justin Willis <jgw9617@gmail.com>
2020-10-28 13:53:52 -0700

    refactor(): small refactor to web manifest function

--- WebManifest/index.ts
+++ WebManifest/index.ts
@@ -1,57 +1,19 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-import { checkBackgroundColor, checkCategories, checkDesc, checkDisplay, checkIcons, checkMaskableIcon, checkName, checkOrientation, checkRating, checkRelatedApps, checkRelatedPref, checkScreenshots, checkShortName, checkStartUrl, checkThemeColor } from './mani-tests';
 import getManifest from "../utils/getManifest";
+import testManifest from "../utils/testManifest";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  context.log(`Web Manifest function is processing a request for site: ${req.query.site}`);
+  context.log(`Web Manifest function is processing a request for site: ${req?.query?.site}`);
 
-  let site = null;
-  let maniObject = null;
-  let maniUrl = null;
-
-  // set our variables to actual values
-  // if the values are available
-  if (req.query && req.query.site) {
-    site = req.query.site;
-  }
-
-  if (req.body && req.body.manifest) {
-    maniObject = req.body.manifest;
-  }
-
-  if (req.body && req.body.maniurl) {
-    maniUrl = req.body.maniurl;
-  }
+  const site = req?.query?.site;
+  const maniObject = req?.body?.manifest;
+  const maniUrl = req?.body?.maniurl;
 
   try {
-
     if (maniObject && (maniUrl || site)) {
-      context.log(`Web Manifest function has a raw manifest object for site: ${req.query.site}`);
+      context.log(`Web Manifest function has a raw manifest object for site: ${req?.query?.site}`);
 
-      const results = {
-        "required": {
-          "short_name": checkShortName(maniObject),
-          "name": checkName(maniObject),
-          "display": checkDisplay(maniObject),
-          "start_url": checkStartUrl(maniObject),
-          "icons": checkIcons(maniObject)
-        },
-        "recommended": {
-          "screenshots": checkScreenshots(maniObject),
-          "description": checkDesc(maniObject),
-          "categories": checkCategories(maniObject),
-          "maskable_icon": checkMaskableIcon(maniObject),
-          "iarc_rating": checkRating(maniObject),
-          "related_applications": checkRelatedApps(maniObject),
-          "prefer_related_applications": checkRelatedPref(maniObject),
-          "background_color": checkBackgroundColor(maniObject),
-          "theme_color": checkThemeColor(maniObject),
-          "orientation": checkOrientation(maniObject)
-        },
-        "optional": {
-
-        }
-      }
+      const results = await testManifest(maniObject);
 
       context.res = {
         status: 200,
@@ -71,30 +33,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
       const maniData = await getManifest(site);
 
       if (maniData) {
-        const results = {
-          "required": {
-            "short_name": checkShortName(maniData.json),
-            "name": checkName(maniData.json),
-            "display": checkDisplay(maniData.json),
-            "start_url": checkStartUrl(maniData.json),
-            "icons": checkIcons(maniData.json)
-          },
-          "recommended": {
-            "screenshots": checkScreenshots(maniData.json),
-            "description": checkDesc(maniData.json),
-            "categories": checkCategories(maniData.json),
-            "maskable_icon": checkMaskableIcon(maniData.json),
-            "iarc_rating": checkRating(maniData.json),
-            "related_applications": checkRelatedApps(maniData.json),
-            "prefer_related_applications": checkRelatedPref(maniData.json),
-            "background_color": checkBackgroundColor(maniData.json),
-            "theme_color": checkThemeColor(maniData.json),
-            "orientation": checkOrientation(maniData.json)
-          },
-          "optional": {
-
-          }
-        }
+        const results = await testManifest(maniObject);
 
         context.res = {
           status: 200,
