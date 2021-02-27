00165f42d8d0e23edf43fe9240bc8bb1f25e976c
lee-leonardo <lee.leonardo.king@gmail.com>
2021-02-26 18:50:00 -0800

    keep the same settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'eslint-plugin-import',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "eslint-plugin-import"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-    'plugin:eslint-plugin-import/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:eslint-plugin-import/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
   ],
 };
