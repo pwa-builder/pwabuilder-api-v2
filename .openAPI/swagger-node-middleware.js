aba35587c2e486d7d251445e31ad83eb758b56f0
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2022-11-04 21:14:29 +0100

    middleware propotype

--- .openAPI/swagger-node-middleware.js
+++ .openAPI/swagger-node-middleware.js
@@ -1,6 +1,12 @@
+const express = require("express")
+const httpProxy = require('http-proxy')
+const apiProxy = httpProxy.createProxyServer(
+  {target: { host: '0.0.0.0', port: 7071, protocol: 'http' }, changeOrigin: true}
+)
+
 const fs = require("fs")
 const path = require('path')
-const express = require("express")
+
 const pathToSwaggerUi = require("swagger-ui-dist").absolutePath()
 
 const indexContent = fs.readFileSync(`${pathToSwaggerUi}/swagger-initializer.js`)
@@ -11,5 +17,9 @@ const app = express()
 app.get("/swagger-initializer.js", (req, res) => res.send(indexContent))
 app.use(express.static(pathToSwaggerUi))
 app.use('/openAPI', express.static(path.join(__dirname, '.')))
+
+app.all(/\/api\/*/, function(req, res) {
+  apiProxy.web(req, res);
+});
 
 app.listen(80)
