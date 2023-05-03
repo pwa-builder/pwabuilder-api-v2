3704407726713702ac80dc57b42a2a31f35897de
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2023-05-03 14:35:47 +0200

    adjust config for report artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,13 +26,13 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { outputFolder: 'temp' }],
-    ['json', { outputFile: 'temp/test_results.json' }]
+    ['html', { outputFolder: 'temp/report/' }],
+    ['json', { outputFile: 'temp/report/test_results.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -86,7 +86,7 @@ export default defineConfig({
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'temp/artifacts',
+  outputDir: 'temp/artifacts',
 
   /* Run your local dev server before starting the tests */
   webServer: {
